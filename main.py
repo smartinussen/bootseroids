5423e5936b99db9eaa97bc720a362428f622f280
smartinussen <steinar.martinussen@icloud.com>
2025-05-05 09:41:43 +0200

    Updated with Clock to restrict the game to 60fps

--- main.py
+++ main.py
@@ -3,13 +3,16 @@
 # throughout this file
 import pygame, constants
 
-pygame.init()
-screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -17,6 +20,8 @@ def main():
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
