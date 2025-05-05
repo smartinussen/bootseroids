91db894198c17ad0b543083e171211e7709127b7
smartinussen <steinar.martinussen@icloud.com>
2025-05-05 09:33:02 +0200

    Drawing the screen

---
+++ main.py
@@ -0,0 +1,22 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame, constants
+
+pygame.init()
+screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
